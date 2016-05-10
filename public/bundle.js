81cefc966e019e79afdae169e83f668ae34095a3
SnowDragon <stan.benavides@icloud.com>
2016-05-10 12:44:51 -0400

    prep repo for Heroku

--- public/bundle.js
+++ public/bundle.js
@@ -59,7 +59,7 @@
 	var Main = __webpack_require__(216);
 	var Weather = __webpack_require__(218);
 	var About = __webpack_require__(240);
-	var Examples = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"Examples\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+	var Examples = __webpack_require__(241);
 	var WeatherForm = __webpack_require__(219);
 	//var WeatherMessage = require("WeatherMessage");
 
@@ -26234,6 +26234,41 @@
 	};
 
 	module.exports = About;
+
+/***/ },
+/* 241 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+
+	var React = __webpack_require__(1);
+
+	// var Examples = React.createClass({
+	//   render: function () {
+	//     return (
+	//       <h3>Examples Component</h3>
+	//     )
+	//   }
+	// });
+
+	var Examples = function Examples(props) {
+	  return React.createElement(
+	    "div",
+	    null,
+	    React.createElement(
+	      "h3",
+	      null,
+	      "Examples Component"
+	    ),
+	    React.createElement(
+	      "p",
+	      null,
+	      "Welcome to examples page!"
+	    )
+	  );
+	};
+
+	module.exports = Examples;
 
 /***/ }
 /******/ ]);
